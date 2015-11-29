4409512a7e34e9c776411bd3b540c97358a2c880
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2015-11-29 17:16:05 +0100

    Update Gemfile

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,8 +4,6 @@ require 'coveralls'
 Coveralls.wear!
 require 'simplecov'
 SimpleCov.start
-require 'codeclimate-test-reporter'
-CodeClimate::TestReporter.start
 
 RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
